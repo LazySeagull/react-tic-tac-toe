2f222814a93178160a561cb67dffdee7df23a85e
Ateeksh <ateesood@gmail.com>
2025-10-19 20:47:10 +0000

    implemented the clear function and added a wrapper

--- react-tic-tac-toe-yeah/src/App.jsx
+++ react-tic-tac-toe-yeah/src/App.jsx
@@ -40,24 +40,35 @@ export default function Board() {
     status = "Next turn : " + (xIsNext ? "X" : "O");
   }
 
+  function clear(squares){
+    setSquares(Array(9).fill(null));
+    
+  }
 
   return (
     <>
       <div className = "status">{status}</div>
-      <div className="board-row">
-        <Square value = {squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareClick={() => handleClick(2)}/>
+      
+      <div className = "board">
+        <div className="board-row">
+          <Square value = {squares[0]} onSquareClick={() => handleClick(0)}/>
+          <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
+          <Square value = {squares[2]} onSquareClick={() => handleClick(2)}/>
+        </div>
+        <div className="board-row">
+          <Square value = {squares[3]} onSquareClick={() => handleClick(3)}/>
+          <Square value = {squares[4]} onSquareClick={() => handleClick(4)}/>
+          <Square value = {squares[5]} onSquareClick={() => handleClick(5)}/>
+        </div>
+        <div className="board-row">
+          <Square value = {squares[6]} onSquareClick={() => handleClick(6)}/>
+          <Square value = {squares[7]} onSquareClick={() => handleClick(7)}/>
+          <Square value = {squares[8]} onSquareClick={() => handleClick(8)}/>
+        </div>
       </div>
-      <div className="board-row">
-        <Square value = {squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value = {squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value = {squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value = {squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value = {squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value = {squares[8]} onSquareClick={() => handleClick(8)}/>
+
+      <div className = "clear">
+        <button onClick={() => clear(squares)}>clear</button>
       </div>
     </>
   );
